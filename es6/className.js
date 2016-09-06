14acb62e994e2506c02b51a89a180c73c4ad89f6
BingKui <178974662@qq.com>
2016-09-06 15:08:11 +0700

    add

---
+++ es6/className.js
@@ -0,0 +1,11 @@
+/*
+ * 不定参数返回样式class属性值
+ */
+module.exports = (...args) => {
+	let _arr = [];
+	for(let arg of args) {
+		let _item  = arg instanceof Array ? arg : (arg || "").split(" ");
+		_arr = _arr.concat(_item);
+	}
+	return _arr.join(" ");
+}
